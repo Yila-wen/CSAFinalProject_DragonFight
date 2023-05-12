e1a758f53dfe0a6b445be814f90a505ca58c6b44
BT_4N2_28 <yila-wen>
2023-05-12 12:34:27 -0400

    Added DirtTile and StoneTile

--- src/tiles/TileManager.java
+++ src/tiles/TileManager.java
@@ -23,6 +23,10 @@ public class TileManager {
         try{
             tile[0] = new Tile();
             tile[0].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/GrassTile.png")));
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/DirtTile.png")));
+            tile[2] = new Tile();
+            tile[2].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/StoneTile.png")));
         }catch (IOException e){
             e.printStackTrace();
         }
@@ -30,8 +34,7 @@ public class TileManager {
 
         // FINISH
         public void draw(Graphics2D g2){
-
-        g2.drawImage(tile[0].image,0,400,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[0].image,0,400,gp.tileSize,gp.tileSize,null);
             g2.drawImage(tile[0].image,48,400,gp.tileSize,gp.tileSize,null);
             g2.drawImage(tile[0].image,96,400,gp.tileSize,gp.tileSize,null);
             g2.drawImage(tile[0].image,144,400,gp.tileSize,gp.tileSize,null);
@@ -40,7 +43,37 @@ public class TileManager {
             g2.drawImage(tile[0].image,288,400,gp.tileSize,gp.tileSize,null);
             g2.drawImage(tile[0].image,336,400,gp.tileSize,gp.tileSize,null);
             g2.drawImage(tile[0].image,384,400,gp.tileSize,gp.tileSize,null);
-            g2.drawImage(tile[0].image,432,400,gp.tileSize,gp.tileSize,null);
+
+            g2.drawImage(tile[1].image,0,448,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,48,448,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,96,448,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,144,448,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,192,448,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,240,448,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,288,448,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,336,448,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,384,448,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,0,496,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,48,496,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,96,496,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,144,496,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,192,496,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,240,496,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,288,496,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,336,496,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,384,496,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,0,520,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,48,520,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,96,520,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,144,520,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,192,520,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,240,520,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,288,520,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,336,520,gp.tileSize,gp.tileSize,null);
+            g2.drawImage(tile[1].image,384,520,gp.tileSize,gp.tileSize,null);
+
+            g2.drawImage(tile[2].image,432,448,gp.tileSize,gp.tileSize,null);
+
         }
 
     }
